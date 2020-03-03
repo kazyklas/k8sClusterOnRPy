3f7417407d96fe4b7341315ce855222f2c07a037
Tomas Klas <tomas.klas@datamole.cz>
2020-03-03 22:55:51 +0100

    Add uploading files

---
+++ src/WebInterface/server.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, flash, request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'hashstaged'
+ALLOWED_EXTENSIONS = {'txt'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect('/')
+
+    return render_template('home.html')
+
+# If we're running in stand alone mode, run the application
+if __name__ == '__main__':
+    app.run(debug=True)
