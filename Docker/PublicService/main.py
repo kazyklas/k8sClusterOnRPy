b1f2fef498fdd1c1bc8943d95884afe556a708c9
Tomas Klas <tomas.klas@datamole.cz>
2020-03-05 22:29:55 +0100

    Add some comments to the main.py

--- Docker/PublicService/main.py
+++ Docker/PublicService/main.py
@@ -3,12 +3,43 @@ from fastapi import FastAPI, Form, File, UploadFile
 from fastapi.responses import HTMLResponse
 import simplejson
 
+"""
+Creator: Tomas Klas
+Contact: tomas.klas@seznam.cz
+Date: 2020-03-05
+"""
+
+
 app = FastAPI()
 
 
+# Package          Version
+# ---------------- -------
+# Click            7.0    
+# fastapi          0.52.0 
+# h11              0.9.0  
+# httptools        0.1.1  
+# pip              20.0.2 
+# psycopg2         2.8.4  
+# pydantic         1.4    
+# python-multipart 0.0.5  
+# setuptools       45.2.0 
+# simplejson       3.17.0 
+# six              1.14.0 
+# starlette        0.13.2 
+# uvicorn          0.11.3 
+# uvloop           0.14.0 
+# websockets       8.1    
+# wheel            0.34.2 
+
+
+# Create_file is a function to get the file with hashes from user and update DB
+# with data that are in the file.
+# Also it will get the name of the hash for future cracking.
 @app.post("/files/")
 async def create_file(files: UploadFile = File(...), hashtype: str = Form(...)):
     try:
+        # connect to DB. TODO change ip address to the hostname of the DB.
         connection = psycopg2.connect(database="cracking",
                                       user="postgres",
                                       password="password1",
@@ -16,7 +47,10 @@ async def create_file(files: UploadFile = File(...), hashtype: str = Form(...)):
                                       port="5432")
 
         cursor = connection.cursor()
+        
+        # TODO check file extension and hashtype that are supported.
 
+        # Read file line by line and generate SQL query.
         while True:
             line = files.file.readline().decode("UTF-8")
             if line == '':
@@ -26,6 +60,7 @@ async def create_file(files: UploadFile = File(...), hashtype: str = Form(...)):
                 data = (line, hashtype, "False", "NULL")
                 cursor.execute(SQL_query, data)
 
+        # Commit the changes to hte database and return success.
         connection.commit()
         return {
             "Database Fill": "Success"
@@ -37,20 +72,27 @@ async def create_file(files: UploadFile = File(...), hashtype: str = Form(...)):
         }
 
 
+# This function will return the set of the hashes that are in the DB.
+# It will show ale the data that are in the DB.
+# Not solving which user added or another stuff like that.
+# TODO Try to monetize this in the diploma thesis
 @app.get("/solved")
 async def print_database():
     try:
+        # Connect to DB. TODO change ip address to the hostname of the DB.
         connection = psycopg2.connect(database="cracking",
                                       user="postgres",
                                       password="password1",
                                       host="127.0.0.1",
                                       port="5432")
 
+        # Get the data from DB
         SQL_query = "select * from hashes"
         cursor = connection.cursor()
         cursor.execute(SQL_query)
         records = cursor.fetchall()
-
+        
+        # Generate dict for future jsoning and responding
         hashes = []
         for row in records:
              hash = {
@@ -80,4 +122,4 @@ async def main():
 </body>
     """
     return HTMLResponse(content=content)
-1
+
